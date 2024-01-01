324c2803493084b9292fe3e70bbb67bdd7d656f6
Ediblelnk <peters1731@gmail.com>
2023-12-31 19:10:50 -0500

    title, game flow :)

--- TODO.md
+++ TODO.md
@@ -1,8 +1,9 @@
-# Things to Do
+# Things I Would Like To Do
 
 - [x] Replace rotate_l/r with just >> whenever possible
 - [x] Improve the evaluation function for position
 - [x] Fix the move generation by filtering only for legal moves (i.e. king safety)
 - [ ] Improve the efficiency of alpha-beta pruning by ordering the moves before searching
-- [ ] Implement a user input so people can play against the machine
+- [x] Implement a user input so people can play against the machine
 - [ ] Improve documentation for ease of use and understandability
+- [ ] Compute the evaluation of moves in parallel
--- terminal-chess/src/main.rs
+++ terminal-chess/src/main.rs
@@ -1,23 +1,5 @@
-use std::time::Instant;
-// use std::{thread, time};
 use terminal_chess as lib;
 
 fn main() {
-    let mut c = lib::Chess::new();
-    c.print_board();
-    let mut buf = String::new();
-    loop {
-        let _ = std::io::stdin().read_line(&mut buf);
-
-        let start = Instant::now();
-        let x = c.minimax(5, isize::MIN + 2, isize::MAX - 1);
-        let duration = start.elapsed();
-
-        println!("Time: {:?}", duration);
-        println!("{:?}\n", x);
-        println!("Eval: {}", c.evaluate());
-        c.move_piece(x.1.unwrap());
-        println!("Legal Moves: {:?}", c.get_legal_moves());
-        c.print_board();
-    }
+    lib::Chess::new().play();
 }
